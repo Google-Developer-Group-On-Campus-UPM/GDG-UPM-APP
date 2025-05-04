44141c71cb67db6718321754019d401e760f66e8
FireRei <77768589+FireRei@users.noreply.github.com>
2025-05-04 21:07:44 +0800

    Added basic firestore create/set functions for teams and members

---
+++ app/functions/firebase.tsx
@@ -0,0 +1,46 @@
+import { db } from "@/config/firebase-config"
+import { doc, collection, getDoc, getDocs, setDoc, addDoc, updateDoc, deleteDoc, query, where, QuerySnapshot, CollectionReference } from "firebase/firestore"
+
+interface TeamMember {
+	name: string;
+	role: string;
+	image: string;
+	social: {
+		facebook: string;
+		twitter: string;
+		github: string;
+	};
+	imagePositions: string;
+}
+
+async function addMemberToTeam(teamId: string, data: TeamMember): Promise<void> {
+	try {
+		const teamRef = await collection(doc(db, "team", teamId), "members");
+		const memberExists = (await getDocs(teamRef)).docs.findIndex((doc) => doc.data().name === data.name);
+		if (memberExists) {
+			console.log("Member already exists in the team.");
+			return;
+		}
+		await addDoc(teamRef, data);
+		console.log("Member added to team successfully.");
+	} catch (error) {
+		console.error("Error adding member to team: ", error);
+	}
+	return;
+}
+
+async function addTeam(data: TeamMember): Promise<void> {
+	try {
+		const teamRef = await collection(db, "team");
+		const teamExists = ((await getDocs(teamRef)).docs).findIndex((doc) => doc.data().name === data.name);
+		if (teamExists) {
+			console.log("Team already exists in the database.");
+			return;
+		}
+		await addDoc(teamRef, data);
+		console.log("Team added to database successfully.");
+	} catch (error) {
+		console.error("Error adding team to database: ", error);
+	}
+	return;
+}
