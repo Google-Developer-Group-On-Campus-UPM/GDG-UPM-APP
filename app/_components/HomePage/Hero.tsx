97d267384b2d01587dcb9160fd34a3623734cdb9
azzumar247 <azzumar.tbh@gmail.com>
2025-06-11 23:58:09 +0800

    added cards and add border gradient

--- app/_components/HomePage/Hero.tsx
+++ app/_components/HomePage/Hero.tsx
@@ -3,6 +3,7 @@
 import { Poppins } from "next/font/google"
 import React, { useState, useEffect } from "react"
 import Link from "next/link"
+import styles from "./Hero.module.css"
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
@@ -10,6 +11,24 @@ const poppins = Poppins({
 })
 
 const words = ["Developers", "Designers"]
+
+const cards = [
+  {
+    icon: "/images/hero/connect-icon.svg",
+    title: "Connect",
+    description: "Collaborate on projects, and  grow lasting friendships.",
+  },
+  {
+    icon: "/images/hero/learn-icon.svg",
+    title: "Learn",
+    description: "From workshops to hands-on sessions  — always something new to explore.",
+  },
+  {
+    icon: "/images/hero/grow-icon.svg",
+    title: "Grow",
+    description: "Unlock opportunities, and grow as a tech leader.",
+  },
+]
 
 
 const Hero = () => {
@@ -45,56 +64,73 @@ const Hero = () => {
 
   return (
     <div className="min-h-screen">
-      <div className={"space-y-10"}>
+      <div className={"space-y-20"}>
 
-        <div className={"flex justify-center w-full"}>
-          <div className={"border border-gray-200 backdrop-blur-md rounded-full h-8 py-2 px-6 bg-gray-300/20 mt-52 flex items-center justify-center"}>
-            <div className={"flex items-center justify-center gap-2"}>
-              <img src="/images/hero/hat-icon.svg" alt="" />
-              <span>Student Developer Club</span>
-              <span>-</span>
-              <Link href={"/"} className={"text-white italic hover:underline"}>
-                Learn More
-              </Link>
+        <div className={"space-y-10"}>
+          <div className={"flex justify-center w-full"}>
+            <div className={`${styles.gradientBorder} mt-52`}>
+              <div className={"backdrop-blur-md rounded-full py-2 px-6 bg-gray-300/20 flex items-center justify-center"}>
+                <div className={"flex items-center justify-center gap-2"}>
+                  <img src="/images/hero/hat-icon.svg" alt="" />
+                  <span>Student Developer Club</span>
+                  <span>-</span>
+                  <Link href={"/"} className={"text-white italic hover:underline"}>
+                    Learn More
+                  </Link>
+                </div>
+              </div>
             </div>
-
           </div>
-        </div>
 
-        <div className={"space-y-3"}>
+          <div className={"space-y-3"}>
 
-          <h1 className={`${poppins.className} text-white text-8xl text-center font-medium`}>
-            <span
-              className="inline-block relative
+            <h1 className={`${poppins.className} text-white text-[5rem] leading-none text-center font-medium`}>
+              <span
+                className="inline-block relative
                    [mask-image:linear-gradient(to_right,rgba(0,0,0,0.3)_1%,rgba(0,0,0,1)_99%,rgba(0,0,0,1)_100%)]
                    [-webkit-mask-image:linear-gradient(to_right,rgba(0,0,0,0.3)_1%,rgba(0,0,0,1)_99%,rgba(0,0,0,1)_100%)]
                    [mask-mode:alpha] [-webkit-mask-mode:alpha]"
-            >
-              Your #1 Tech Community
-            </span>
-            <br />
+              >
+                Your #1 Tech Community
+              </span>
+              <br />
 
-            <span
-              className="inline-block relative leading-[1.2] pb-2
+              <span
+                className="inline-block relative leading-[1.2] pb-2
              [mask-image:linear-gradient(to_right,rgba(0,0,0,0.3)_1%,rgba(0,0,0,1)_99%,rgba(0,0,0,1)_100%)]
              [-webkit-mask-image:linear-gradient(to_right,rgba(0,0,0,0.3)_1%,rgba(0,0,0,1)_99%,rgba(0,0,0,1)_100%)]
              [mask-mode:alpha] [-webkit-mask-mode:alpha]"
-            >
-              for <span className={"italic"}>{displayed}<span className="border-r-2 border-white animate-pulse ml-1" /></span>
-            </span>
-          </h1>
+              >
+                for <span className={"italic"}>{displayed}<span className="border-r-2 border-white animate-pulse ml-1" /></span>
+              </span>
+            </h1>
 
-          <p className={"text-lg text-gray-400 font-light text-center"}>An open-space community where ideas thrive, skills grow, and <br />everyone belongs — welcome to GDGoC UPM.</p>
+            <p className={"text-lg text-gray-400 font-light text-center"}>An open-space community where ideas thrive, skills grow, and <br />everyone belongs — welcome to GDGoC UPM.</p>
 
+          </div>
         </div>
 
         {/* Cards */}
-        <div className={"grid grid-cols-1 md:grid-cols-3 gap-4"}>
-          <div className={""}>
-
-          </div>
-
-
+        <div className={"mx-12 sm:mx-14 md:mx-20 lg:mx-64 grid grid-cols-1 md:grid-cols-3 gap-4"}>
+          {cards.map((card, idx) => (
+            <div
+              key={card.title}
+              className={`p-[1px] rounded-lg relative bg-gradient-to-r from-[#67A4D5] to-[#ADEDFF] cursor-pointer`}
+            >
+              <div className="bg-gradient-to-r from-[#06233B] to-[#09365b] rounded-lg text-white space-y-1 h-full">
+                <div className={"flex items-center justify-end pt-2 pr-2"}>
+                  <img src="/images/hero/hover-action.svg" alt="" />
+                </div>
+                <div className={"pb-6 pr-6 pl-6"}>
+                  <div className={"flex"}>
+                    <img src={card.icon} alt="" />
+                    <span className={"text-xl ml-2"}>{card.title}</span>
+                  </div>
+                  <p className={"text-gray-400 font-light text-lg"}>{card.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
 
 
