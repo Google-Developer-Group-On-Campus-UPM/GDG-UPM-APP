b56873139967e58ccfa541791bd6dc12bd694be7
FireRei <77768589+FireRei@users.noreply.github.com>
2025-06-15 23:34:21 +0800

    Added CRUD firebase functions for team, check usage documentation

--- app/functions/firebase.tsx
+++ app/functions/firebase.tsx
@@ -1,161 +1,193 @@
 import { db } from "@/config/firebase-config"
-import { doc, collection, getDoc, getDocs, setDoc, addDoc, deleteDoc, query, where, QuerySnapshot } from "firebase/firestore"
+import { doc, collection, getDoc, getDocs, setDoc, addDoc, deleteDoc, updateDoc, 
+	DocumentData, CollectionReference, QueryDocumentSnapshot, WithFieldValue,
+	DocumentReference, 
+	where,
+	query,
+	QueryFieldFilterConstraint,
+	QueryConstraint} from "firebase/firestore"
 
 // TODO: Test this code with a real firebase project and add types to the functions
 // TODO: Add delete functions for team and team members
 
-interface TeamMember {
-	id: string;
-	name: string;
-	role: string;
-	image: string;
+
+
+
+interface DataWithId extends DocumentData {
+	id?: string;
+}
+interface TeamMember extends DataWithId {
+	name: string | null;
+	role: string | null;
+	image: string | null;
+	interest: string | null;
 	social: {
-		instagram: string;
 		linkedin: string;
-		github: string;
+		[key: string]: string;
 	};
-	imagePositions: string;
+	isActive: boolean;
+	currentRoleID?: string;
+	currentDepartmentID?: string;
 }
 
-interface Team {
-	id: string;
+interface Role extends DataWithId {
+	title: string;
+}
+
+interface Department extends DataWithId {
 	name: string;
-	data: {
-		[key: string]: () => Promise<TeamMember[]>;
-	}
+	description?: string;
 }
 
-async function addMemberToTeam(teamId: string, data: TeamMember): Promise<string> {
-	try {
-		const teamRef = await collection(doc(db, "team", teamId), "members");
-		const memberExists = (await getDocs(teamRef)).docs.findIndex((doc) => doc.data().name === data.name);
-		if (memberExists) {
-			throw new Error("Member already exists in the team.");
-		}
-		await addDoc(teamRef, data);
-		return "Member added to team successfully.";
-	} catch (error) {
-		if (error instanceof Error) {
-			throw new Error("Error adding member to team: " + error.message);
-		}
-		throw new Error("Unknown error adding member to team.");
-	}
-}
+class TeamService {
+	private usersCollection: CollectionReference<DocumentData>;
+	private rolesCollection: CollectionReference<DocumentData>;
+	private departmentsCollection: CollectionReference<DocumentData>;
 
-async function updateMemberInTeam(teamId: string, memberId: string, data: TeamMember, merge?: boolean): Promise<string> {
-	try {
-		await setDoc(doc(db, "team", teamId, "members", memberId), data, { merge });
-		return "Member updated in team successfully.";
-	} catch (error) {
-		if (error instanceof Error) {
-			throw new Error("Error updating member in team: " + error.message);
-		}
-		throw new Error("Unknown error updating member in team.");
+	constructor() {
+		this.usersCollection = collection(db, "users");
+		this.rolesCollection = collection(db, "roles");
+		this.departmentsCollection = collection(db, "departments");
 	}
-}
 
-async function addTeam(data: TeamMember, team?: QuerySnapshot): Promise<string> {
-	try {
-		const teamCollectionRef = await collection(db, "team");
-		if (!team) {
-			team = (await getDocs(teamCollectionRef));
-		}
-		const teamExists = (team.docs).findIndex((doc) => doc.data().name === data.name);
-		if (teamExists) {
-			throw new Error("Team already exists in the database");
-		}
-		await addDoc(teamCollectionRef, data);
-		return "Team added to database successfully.";
-	} catch (error) {
-		if (error instanceof Error) {
-			throw new Error("Error adding team to database: " + error.message);
-		}
-		throw new Error("Unknown error adding team to database.");
-	}
-}
+	async getData(
+	collection: CollectionReference<DocumentData>,
+	constraints: QueryConstraint[] = []
+	): Promise<DataWithId[]> {
+		try {
+			const q = constraints.length
+				? query(collection, ...constraints)
+				: collection;
+				
+			const querySnapshot = await getDocs(q);
 
-async function updateTeam(teamId: string, data: any, merge?: boolean): Promise<string> {
-	try {
-		await setDoc(doc(db, "team", teamId), data, { merge });
-		return "Team updated in database successfully.";
-	} catch (error) {
-		if (error instanceof Error) {
-			throw new Error("Error updating team in database: " + error.message);
-		}
-		throw new Error("Unknown error updating team in database");
-	}
-}
-
-async function getTeams(): Promise<Team[]> {
-	try {
-		const teamsRef = await getDocs(collection(db, "team"));
-		const teams = await Promise.all(
-			teamsRef.docs.map(async (doc) => {
-			const data = doc.data();
-			return { 
+			return querySnapshot.docs.map((doc) => ({
 				id: doc.id,
-				name: data.name,
-				data: {
-					[data.name]: () => getMembersOfTeam(doc.id)  
-				}
-			}
-		}));
-		return teams
-	} catch (error) {
-		if (error instanceof Error) {
-			throw new Error("Error fetching teams: " + error.message);
+				...doc.data(),
+			})) as DataWithId[];
+		} catch (error) {
+			console.error("Failed to get documents:", error);
+			throw error;
 		}
-		throw new Error("Unknown error fetching teams");
+	}
+
+	async getById<T>(collection: CollectionReference<T>, id: string): Promise<(T & { id: string }) | null> {
+		try {
+			const docRef = doc(collection, id);
+			const snapshot = await getDoc(docRef);
+			if (!snapshot.exists()) return null;
+			return { id: snapshot.id, ...snapshot.data() };
+		} catch (error) {
+			console.error("Failed to get document by ID:", error);
+			throw error;
+		}
+	}
+
+	async getUsers(query?: QueryConstraint[]): Promise<TeamMember[]> {
+		try {
+			return (await this.getData(this.usersCollection, query)) as TeamMember[];
+		} catch (error) {
+			console.error("Failed to get users: ", error);
+			throw error;
+		}
+	}
+
+	async getRoles(query?: QueryConstraint[]): Promise<Role[]> {
+		try {
+			return (await this.getData(this.rolesCollection, query)) as Role[];
+		} catch (error) {
+			console.error("Failed to get roles: ", error);
+			throw error;
+		}
+	}
+
+	async getDepartments(query?: QueryConstraint[]): Promise<Department[]> {
+		try {
+			return (await this.getData(this.departmentsCollection, query)) as Department[];
+		} catch (error) {
+			console.error("Failed to get departments: ", error);
+			throw error;
+		}
+	}
+
+	async createData<T extends DocumentData>(data: WithFieldValue<T>, collection: CollectionReference<T>): Promise<DocumentReference<T>> {
+		try { 
+			return await addDoc(collection, data);
+		} catch (error) {
+			console.error("Failed to create document:", error);
+			throw error;
+		}
+	}
+
+	async createUser(user: Partial<TeamMember>): Promise<DocumentReference> {
+		const defaultUser: TeamMember = {
+			name: "gdgoc member",
+			role: "member",
+			image: "/images/no-pfp.png",
+			social: {
+				linkedin: "https://www.linkedin.com/company/google-developer-groups-upm/"
+			},
+			interest: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis nesciunt dolores quisquam suscipit sapiente illo illum, rem saepe ullam vitae eligendi corrupti voluptate officiis aspernatur, sequi velit omnis reiciendis iusto?",
+			isActive: true,
+		}
+
+		const finalUser: TeamMember = {
+			...defaultUser,
+			...user
+		}
+		return this.createData(finalUser, this.usersCollection);
+	}
+
+	async createRole(role: Partial<Role>): Promise<DocumentReference> {
+		return this.createData(role, this.rolesCollection);
+	}
+
+
+
+	async createDepartment(department: Partial<Department>): Promise<DocumentReference> {
+		return this.createData(department, this.departmentsCollection);
+	}
+
+	async updateData<T>(data: Partial<T>, documentReference: DocumentReference<T>): Promise<boolean> {
+		try {
+			await updateDoc(documentReference, data);
+			return true;
+		} catch (error) {
+			console.error("Failed to update document:", error);
+			throw error;
+		}
+	}
+
+	async setData<T extends DocumentData>(data: WithFieldValue<T>, documentReference: DocumentReference<T>): Promise<boolean> {
+		try {
+			await setDoc(documentReference, data);
+			return true;
+		} catch (error) {
+			console.error("Failed to update document:", error);
+			throw error;
+		}
+	}
+
+	async deleteData(documentReference: DocumentReference): Promise<boolean> {
+		try {
+			await deleteDoc(documentReference);
+			return true;
+		} catch (error) {
+			console.error("Failed to delete document:", error);
+			throw error;
+		}
+	}
+
+	async deleteAll(collectionReference: CollectionReference): Promise<boolean>  {
+		try {
+			const documents = (await getDocs(collectionReference)).docs;
+			await Promise.all(documents.map((doc) => deleteDoc(doc.ref)));
+			return true;
+		} catch (error) {
+			console.error("Failed to delete all documents:", error);
+			throw error;
+		}
 	}
 }
 
-async function getTeam(name: string, teams?: Team[]): Promise<Object> {
-	try {
-		if (!teams) {
-			teams = await getTeams();
-		}
-		const team = teams.find((team) => team.name === name);
-		if (team) {
-			return team;
-		} else {
-			throw new Error("Team not found.");
-		}
-	} catch (error) {
-		if (error instanceof Error) {
-			throw new Error("Error fetching team: " + error.message);
-		}
-		throw new Error("Unknown error fetching team");
-	}
-} {
-
-} 
-
-async function getMembersOfTeam(teamId: string): Promise<TeamMember[]> {
-	try {
-		const teamDoc = await getDoc(doc(db, "team", teamId));
-		if (!teamDoc.exists()) {
-			throw new Error("Error: Team doesn't exist.");
-		}
-		const membersDocs = await getDocs(collection(teamDoc.ref, "members"));
-		return membersDocs.docs.map((doc) => {
-			const data = doc.data();
-			return { 
-				id: doc.id,
-				name: data.name,
-				role: data.role,
-				image: data.image || "",
-				social: {
-					instagram: data.social?.instagram || "",
-					linkedin: data.social?.linkedin || "",
-					github: data.social?.github || ""
-				},
-				imagePositions: data.imagePositions || ""
-			}
-		});
-	} catch (error) {
-		if (error instanceof Error) {
-			throw new Error("Error fetching team members: " + error.message);
-		}
-		throw new Error("Unknown error fetching team members.");
-	}
-}
+export default TeamService;
