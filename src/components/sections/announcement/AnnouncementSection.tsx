6eada1138a348af2b3fc5fa37abc41d35881e20a
azzumar247 <azzumar.tbh@gmail.com>
2025-06-17 23:01:34 +0800

    update announcement section

--- src/components/sections/announcement/AnnouncementSection.tsx
+++ src/components/sections/announcement/AnnouncementSection.tsx
@@ -4,14 +4,10 @@ import FlexChip from "@/components/ui/FlexChip";
 
 const AnnoucementSection: React.FC = () => {
     return (
-        <div
-            className={
-                "min-h-screen w-full bg-black flex justify-center items-center relative z-0"
-            }
-        >
+        <div className="min-h-screen w-full bg-black flex justify-center items-center relative z-0">
             <Image
                 src="/images/announcement/announcement-bg.svg"
-                className="absolute z-[-10] w-full h-full top-40"
+                className="absolute z-[-10] w-full h-full top-30"
                 width={4000}
                 height={100}
                 alt=""
@@ -20,43 +16,59 @@ const AnnoucementSection: React.FC = () => {
                     maskImage: 'linear-gradient(to bottom, transparent 2%, black 100%)',
                 }}
             />
-            <div className="w-full mx-12 sm:mx-14 md:mx-20 lg:mx-24 xl:mx-54 grid grid-rows-2">
-                <Image
-                    src="/images/hero/Image-Announcement.png"
-                    className=""
-                    width={400}
-                    height={100}
-                    alt="Announcement"
-                />
-                
 
-                
-                {/* <div className="w-full flex-col flex gap-[32px] font-[400]">
-                    <div className="w-fit border border-white rounded-[18px]">
-                        <p className="px-[10px] py-[4px] text-[12px]">announcements</p>
-                    </div>
-                    <div>
-                        <p className="text-[48px]">
+            <div className="mx-4 md:mx-16 lg:mx-32 xl:mx-64 grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-5 items-center max-w-screen-xl w-full">
+                <div className="row-start-2 col-start-1 md:row-span-2 md:col-start-1 flex justify-center md:justify-end">
+                    <Image
+                        src="/images/hero/Image-Announcement.png"
+                        alt="Announcement"
+                        width={800}
+                        height={100}
+                        className="w-full h-auto max-w-md md:max-w-full"
+                    />
+                </div>
+
+                <div className="flex justify-center md:justify-start pt-6 pb-6 md:pb-0 row-start-1 col-start-1 md:col-start-2">
+                    <FlexChip
+                        content={[{ text: "Announcements" }]}
+                        sx={{
+                            lineHeight: "18px",
+                            textAlign: "center",
+                            borderRadius: "9999px",
+                            borderWidth: "1px",
+                            padding: "6px 10px",
+                            gap: "6px",
+                            width: "fit-content",
+                            marginTop: 0,
+                            marginBottom: 0,
+                        }}
+                    />
+                </div>
+
+
+                <div className={"row-start-3 col-start-1 md:row-start-2 md:col-start-2"}>
+                    <div className="space-y-8">
+
+                        <p className="text-3xl font-medium text-white text-center md:text-left">
                             “At GDG, We Commit to{" "}
                             <span className="font-black">
                                 Advancing Theoretical Knowledge and Technical Skills”
                             </span>
                         </p>
-                    </div>
-                    <div>
-                        <p className="text-[20px]">
-                            To nurturing well-rounded developers by offering resources,
-                            mentorship, and a collaborative environment that bridges...
+                        <p className="text-base md:text-lg text-white text-center md:text-left">
+                            To nurturing well-rounded developers by offering resources, mentorship, and a collaborative environment that bridges...
                         </p>
+
+                        <div className={"flex justify-center md:justify-start"}>
+                            <button className="text-white text-base md:text-lg px-8 py-3 rounded-full bg-[radial-gradient(circle,_#515EF3,_#2F21D9)] hover:opacity-90 transition border border-transparent shadow-white shadow">
+                                Read Full Article
+                            </button>
+                        </div>
                     </div>
-                    <div>
-                        <button className="text-white text-[20px] px-[48px] py-[12px] rounded-[30px] bg-[radial-gradient(circle,_#515EF3,_#2F21D9)] hover:opacity-90 cursor-pointer transition border border-transparent shadown-white shadow">
-                            Read Full Article
-                        </button>
-                    </div>
-                </div> */}
+                </div>
             </div>
         </div>
+
     )
 }
 export default AnnoucementSection
