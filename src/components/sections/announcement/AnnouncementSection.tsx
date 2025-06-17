89db74da3033719d62d7bfdbf67554fcf83be47e
azzumar247 <azzumar.tbh@gmail.com>
2025-06-18 00:15:27 +0800

    fix text shrinking when zooming out

--- src/components/sections/announcement/AnnouncementSection.tsx
+++ src/components/sections/announcement/AnnouncementSection.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
-
-import FlexChip from "@/components/ui/FlexChip";
-
+import FlexChip from "@/components/ui/FlexChip"; 
 const AnnouncementSection: React.FC = () => {
     return (
         <div className="min-h-screen w-full bg-black flex justify-center items-center relative z-0">
@@ -16,8 +14,7 @@ const AnnouncementSection: React.FC = () => {
                     maskImage: 'linear-gradient(to bottom, transparent 2%, black 100%)',
                 }}
             />
-
-            <div className="mx-4 md:mx-16 lg:mx-32 xl:mx-64 grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-5 items-center max-w-screen-xl w-full">
+            <div className="mx-4 md:mx-16 lg:mx-32 xl:mx-64 grid grid-cols-1 md:grid-cols-2 grid-rows-[0.2fr_1fr] gap-x-10 gap-y-5 items-center max-w-screen-xl w-full">
                 <div className="row-start-2 col-start-1 md:row-span-2 md:col-start-1 flex justify-center md:justify-end">
                     <Image
                         src="/images/hero/Image-Announcement.png"
@@ -27,7 +24,6 @@ const AnnouncementSection: React.FC = () => {
                         className="w-full h-auto max-w-md md:max-w-full"
                     />
                 </div>
-
                 <div className="flex justify-center md:justify-start pt-6 pb-6 md:pb-0 row-start-1 col-start-1 md:col-start-2">
                     <FlexChip
                         content={[{ text: "Announcements" }]}
@@ -44,22 +40,18 @@ const AnnouncementSection: React.FC = () => {
                         }}
                     />
                 </div>
-
-
-                <div className={"row-start-3 col-start-1 md:row-start-2 md:col-start-2"}>
-                    <div className="space-y-8">
-
-                        <p className="text-3xl font-medium text-white text-center md:text-left">
+                <div className="row-start-3 col-start-1 md:row-start-2 md:col-start-2 self-start">
+                    <div className="flex flex-col justify-between h-full space-y-8">
+                        <p className="font-medium text-white text-center md:text-left text-[clamp(24px,2.5vw,48px)]">
                             “At GDG, We Commit to{" "}
                             <span className="font-black">
                                 Advancing Theoretical Knowledge and Technical Skills”
                             </span>
                         </p>
-                        <p className="text-base md:text-lg text-white text-center md:text-left">
+                        <p className="text-white text-center md:text-left text-[clamp(14px,1.2vw,20px)]">
                             To nurturing well-rounded developers by offering resources, mentorship, and a collaborative environment that bridges...
                         </p>
-
-                        <div className={"flex justify-center md:justify-start"}>
+                        <div className="flex justify-center md:justify-start">
                             <button className="text-white text-base md:text-lg px-8 py-3 rounded-full bg-[radial-gradient(circle,_#515EF3,_#2F21D9)] hover:opacity-90 transition border border-transparent shadow-white shadow">
                                 Read Full Article
                             </button>
@@ -68,7 +60,6 @@ const AnnouncementSection: React.FC = () => {
                 </div>
             </div>
         </div>
-
     )
 }
-export default AnnouncementSection
+export default AnnouncementSection;
