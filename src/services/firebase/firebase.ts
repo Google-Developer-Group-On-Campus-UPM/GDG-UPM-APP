02e9cf2ba58d10b3f31304d2f2fccf46c6d2cffa
FireRei <77768589+FireRei@users.noreply.github.com>
2026-01-08 18:50:33 +0800

    fix: added auth to firebase service

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 import { Firestore, getFirestore } from "firebase/firestore";
 import { FirebaseApp } from "firebase/app";
 import firebaseConfig from "@/config/firebaseConfig";
+import { Auth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,18 +15,21 @@ import firebaseConfig from "@/config/firebaseConfig";
 // Initialize Firebase
 
 let firestore: Firestore | null = null;
+let authInit: Auth;
 
-const initFirebase = (): { db: Firestore } => {
+const initFirebase = (): { db: Firestore; auth: Auth } => {
   if (!firestore) {
     const app: FirebaseApp = initializeApp(firebaseConfig);
     firestore = getFirestore(app);
+    authInit = getAuth(app);
 
     isSupported().then((result) => {
       if (result) getAnalytics(app);
     });
   }
 
-  return { db: firestore };
+  return { db: firestore, auth: authInit };
 };
 
 export const db = initFirebase().db;
+export const auth = initFirebase().auth;
