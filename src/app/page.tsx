e6d961b56049130a1ab53becb68b75dda3cc93fa
oscar-chj <haojchung@gmail.com>
2026-04-05 21:42:39 +0800

    refactor: component name correctness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,15 @@
 "use client";
 
 import Footer from "@/components/layout/Footer";
-import NavBar from "@/components/layout/Navbar";
+import Navbar from "@/components/layout/Navbar";
 import AnnouncementSection from "@/components/sections/announcement/AnnouncementSection";
 import EventsSection from "@/components/sections/events/EventsSection";
 import HeroSection from "@/components/sections/hero/HeroSection";
 
 export default function App() {
 	return (
-		/**
-		 * Navbar Component
-		 * 1. Hero Section
-		 * 2. About Section
-		 * 3. Events Section (from Firebase) -> INPUT: none; OUTPUT: events
-		 * 3. Testimonials Section
-		 * Footer Component
-		 */
-
 		<>
-			<NavBar />
+			<Navbar />
 			<HeroSection />
 			<AnnouncementSection />
 			<EventsSection />
