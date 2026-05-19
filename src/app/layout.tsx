93e84c9e0df6b6df30618999aff4c27ac290eb7b
oscar-chj <haojchung@gmail.com>
2026-05-19 17:47:20 +0800

    feat(seo): configure global SEO metadata with centralized site config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { siteConfig } from "@/config/site";
 import "./globals.css";
 
 // Fonts
@@ -17,8 +18,28 @@ const geistMono = Geist_Mono({
 
 // SEO metadata for the entire application
 export const metadata: Metadata = {
-	title: "GDGoC UPM",
-	description: "Google Developer Group on Campus Universiti Putra Malaysia",
+	title: siteConfig.name,
+	description: siteConfig.description,
+	icons: {
+		icon: "/images/navbar/gdg-logo.svg",
+		shortcut: "/images/navbar/gdg-logo.svg",
+		apple: "/images/navbar/gdg-logo.svg",
+	},
+	openGraph: {
+		title: siteConfig.name,
+		description: siteConfig.description,
+		siteName: siteConfig.name,
+		images: [
+			{
+				url: siteConfig.ogImage,
+				width: 800,
+				height: 600,
+				alt: siteConfig.name,
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
 };
 
 export default function RootLayout({
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+	name: "GDGoC UPM",
+	description: "Google Developer Group on Campus Universiti Putra Malaysia",
+	// TODO: Add production URL here when available, e.g. "https://gdg-upm.com"
+	url: "",
+	ogImage: "/images/navbar/gdg-logo.svg",
+	links: {
+		github: "https://github.com/",
+		instagram: "https://instagram.com/",
+	},
+};
+
+export type SiteConfig = typeof siteConfig;
